ab87e87b778194104215875d7dc01ac68c5ad0ab
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-12-19 17:16:46 +0700

    refactor: change order of require

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . "/App/Router.php";
-require_once __DIR__ . "/Controllers/UserController.php";
 require_once __DIR__ . "/Api/Response.php";
+require_once __DIR__ . "/Controllers/UserController.php";
 
 Router::get('/', [UserController::class, 'users']);
 
