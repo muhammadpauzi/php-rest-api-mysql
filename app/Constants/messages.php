b05a37f31e962545f7420151021152f4d355afea
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-12-20 08:46:38 +0700

    refactor: message api response

---
+++ app/Constants/messages.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Constants;
+
+const SERVER_ERROR_MESSAGE = "SERVER_ERROR";
+
+const USERS_NOT_FOUND_MESSAGE = "USERS_NOT_FOUND";
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -7,6 +7,9 @@ use App\Helpers\Response;
 use App\Respositories\UserRepository;
 use Exception;
 
+use const App\Constants\SERVER_ERROR_MESSAGE;
+use const App\Constants\USERS_NOT_FOUND_MESSAGE;
+
 class UserService
 {
     public function __construct(private UserRepository $userRepository)
@@ -22,7 +25,7 @@ class UserService
             // if users does not exists
             if (!$users)
                 return Response::notFoundResponse([
-                    "message" => "USERS_NOT_FOUND"
+                    "message" => USERS_NOT_FOUND_MESSAGE
                 ]);
             return $users;
             Database::commitTransaction();
@@ -30,7 +33,8 @@ class UserService
             Database::rollbackTransaction();
             return Response::serverErrorResponse(
                 data: [
-                    "message" => $e->getMessage()
+                    "message" => SERVER_ERROR_MESSAGE,
+                    "error" => $e->getMessage()
                 ]
             );
             throw $e;
