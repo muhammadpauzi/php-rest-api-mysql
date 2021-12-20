acec543098054a4b05af9c1ce1bcb6efd7b6dc6d
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-12-20 08:42:52 +0700

    create constants

---
+++ app/Constants/database.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Constants;
+
+const DB_HOST = getenv("DB_HOST");
+const DB_PORT = getenv("DB_PORT");
+const DB_NAME = getenv("DB_NAME");
+const DB_USERNAME = getenv("DB_USERNAME");
+const DB_PASSWORD = getenv("DB_PASSWORD");
