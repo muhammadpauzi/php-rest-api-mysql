e33f38c2e970efbd77fd5047679a7e89b2505059
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-12-31 15:11:45 +0700

    fix: response of posts

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -14,17 +14,17 @@ class PostRepository
 
     public function findAll(): array
     {
-        return $this->db->query("SELECT * FROM $this->table JOIN users ON (users.id = $this->table.id_user)")->resultArray();
+        return $this->db->query("SELECT *, users.created_at AS user_created_at FROM $this->table JOIN users ON (users.id = $this->table.id_user)")->resultArray();
     }
 
     public function findAllBySearchKeyword(string $search_keyword = ""): array
     {
-        return $this->db->query("SELECT * FROM $this->table  JOIN users ON (users.id = $this->table.id_user) WHERE MATCH(title, description) AGAINST(:keyword IN NATURAL LANGUAGE MODE);")->bind(":keyword", $search_keyword)->resultArray();
+        return $this->db->query("SELECT *, users.created_at AS user_created_at FROM $this->table  JOIN users ON (users.id = $this->table.id_user) WHERE MATCH(title, description) AGAINST(:keyword IN NATURAL LANGUAGE MODE);")->bind(":keyword", $search_keyword)->resultArray();
     }
 
     public function findById(int $id): array
     {
-        return $this->db->query("SELECT * FROM $this->table JOIN users ON (users.id = $this->table.id_user) WHERE $this->table.id = :id LIMIT 1")->bind(":id", $id)->single();
+        return $this->db->query("SELECT *, users.created_at AS user_created_at FROM $this->table JOIN users ON (users.id = $this->table.id_user) WHERE $this->table.id = :id LIMIT 1")->bind(":id", $id)->single();
     }
 
     public function findAllByUserID(int $id): array
